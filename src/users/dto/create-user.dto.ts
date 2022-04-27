afec327b445b89caaf87399898553d6448d915ce
lariosd <davidlarios450@gmail.com>
2022-04-27 16:08:45 -0500

    *Add implementation of DTO in main.ts and Change users DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,25 @@
+import {IsEmail, IsNotEmpty, IsString, MaxLength, MinLength} from 'class-validator';
 export class CreateUserDto {
-    userName: string;
-    lastName: string;
-    emailAddress: string;
-    password: string;
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(8)
+    @MaxLength(25)
+    public userName!: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(8)
+    @MaxLength(25)
+    public lastName!: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    public emailAddress!: string;
+
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(7)
+    @MaxLength(25)
+    public password!: string;
 }
