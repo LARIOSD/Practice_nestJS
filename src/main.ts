2991bb79d2f5f9c59967c5c6529b9dceb34906e3
lariosd <davidlarios450@gmail.com>
2022-04-28 16:09:32 -0500

    *Add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger();
 
+  app.enableCors({origin: '*'})
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(4002);
   logger.log(`Server is running in ${await app.getUrl()}`);
