1038eef80d5ba6ea845e99b7f3e4682b6c939059
lariosd <davidlarios450@gmail.com>
2022-04-28 16:12:08 -0500

    *Change UserDto

--- src/api/users/dto/create-user.dto.ts
+++ src/api/users/dto/create-user.dto.ts
@@ -1,26 +1,31 @@
-import {IsEmail, IsNotEmpty, IsString, MaxLength, MinLength} from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
-    @IsNotEmpty()
-    @IsString()
-    @MinLength(8)
-    @MaxLength(25)
-    public userName!: string;
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(25)
+  public userName!: string;
 
-    @IsNotEmpty()
-    @IsString()
-    @MinLength(8)
-    @MaxLength(25)
-    public lastName!: string;
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(25)
+  public lastName!: string;
 
-    @IsEmail()
-    @IsNotEmpty()
-    public emailAddress!: string;
+  @IsEmail()
+  @IsNotEmpty()
+  public emailAddress!: string;
 
-
-    @IsNotEmpty()
-    @IsString()
-    @MinLength(8)
-    @MaxLength(25)
-    public password!: string;
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(25)
+  public password!: string;
 }
