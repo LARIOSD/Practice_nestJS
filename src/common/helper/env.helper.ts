c997d4e73d3349df2bc41988078140b4b3eddd72
lariosd <davidlarios450@gmail.com>
2022-04-28 16:10:29 -0500

    *Delete console.log()

--- src/common/helper/env.helper.ts
+++ src/common/helper/env.helper.ts
@@ -5,17 +5,13 @@ export function getEnvPath(dest: string): string {
   const env: string | undefined = process.env.NODE_ENV;
   const fallback: string = resolve(`${dest}/.env`);
   const filename: string = env ? `${env}.env` : 'development.env';
-  console.log('filename',filename);
   
   let filePath: string = resolve(`${dest}/${filename}`);
-  console.log('filepoath',filePath);
   
 
   if (!existsSync(filePath)) {
     filePath = fallback;
   }
-
-  console.log('holis',filePath);
   
   return filePath;
 }
